47e8735eae9159a1ee40425a930ec197476a5c4c
Joseph Robinson <smartech56@outlook.com>
2019-02-27 11:49:15 -0700

    Pushed browser module changes

--- Browser/Browser/Form1.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Form1
-    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
-        Application.Exit()
-    End Sub
-End Class
